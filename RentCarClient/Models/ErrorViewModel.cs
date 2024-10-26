1e1a014818ef0afab7429a85e3f773f9cddb9b8d
dimasutamaa <dimasutamapa@gmail.com>
2024-10-26 23:28:08 +0700

    Add project files.

---
+++ RentCarClient/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace RentCarClient.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ RentCarClient/wwwroot/js/script.js
@@ -0,0 +1,28 @@
+﻿function calculateDifference() {
+    var pickupDate = new Date(document.getElementById("pickup-date").value);
+    var returnDate = new Date(document.getElementById("return-date").value);
+
+    if (isNaN(pickupDate.getTime()) || isNaN(returnDate.getTime())) {
+        document.getElementById("result").textContent = "Please select both dates.";
+        return;
+    } else {
+        document.getElementById("result").textContent = "";
+    }
+
+    var timeDiff = Math.abs(returnDate.getTime() - pickupDate.getTime());
+    var daysDiff = Math.ceil(timeDiff / (1000 * 3600 * 24));
+
+    var resultText = `Difference: ${daysDiff} day${daysDiff !== 1 ? 's' : ''}`;
+
+    console.log(resultText)
+    console.log(pickupDate)
+    console.log(returnDate)
+}
+
+function openNav() {
+    document.getElementById("mySidenav").style.width = "300px";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
