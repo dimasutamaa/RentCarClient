37efa7d99bfab3efbaee97e91f0d4442538494be
dimasutamaa <dimasutamapa@gmail.com>
2024-10-27 11:59:15 +0700

    add history page

---
+++ RentCarClient/Controllers/RentalController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RentCarClient.Controllers
+{
+    public class RentalController : Controller
+    {
+        // GET: RentalController
+        public IActionResult History()
+        {
+            return View();
+        }
+    }
+}
